3d7b2313e4746ecd189f9d2a24ad29515de0a845
waseemfaruqi <37653216+waseemfaruqi@users.noreply.github.com>
2019-09-19 12:12:34 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const webcamElement = document.getElementById('webcam');
+const classifier = knnClassifier.create();
 let net;
 
 async function app() {
@@ -9,13 +10,34 @@ async function app() {
   console.log('Sucessfully loaded model');
 
   await setupWebcam();
-  while (true) {
-    const result = await net.classify(webcamElement);
-    document.getElementById('console').innerText = `
-      prediction: ${result[0].className}\n
-      probability: ${result[0].probability}
-    `;
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = classId => {
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(webcamElement, 'conv_preds');
 
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+  };
+  document.getElementById('class-a').addEventListener('click', () => addExample(0));
+  document.getElementById('class-b').addEventListener('click', () => addExample(1));
+  document.getElementById('class-c').addEventListener('click', () => addExample(2));
+  document.getElementById('class-d').addEventListener('click', () => addExample(3));
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+        // Get the activation from mobilenet from the webcam.
+        const activation = net.infer(webcamElement, 'conv_preds');
+        // Get the most likely class and confidences from the classifier module.
+        const result = await classifier.predictClass(activation);
+
+        const classes = ['A', 'B', 'C', 'D'];
+        document.getElementById('console').innerText = `
+          prediction: ${classes[result.classIndex]}\n
+          probability: ${result.confidences[result.classIndex]}
+        `;
+      }
     // Give some breathing room by waiting for the next animation frame to
     // fire.
     await tf.nextFrame();
